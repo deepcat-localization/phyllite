8f9bdf13e15cbfc9c3f3bd0e890646e02947573b
ba144220 <hs41511@gmail.com>
2024-06-14 17:49:25 +0800

    update

--- package/components/phyllite.tsx
+++ package/components/phyllite.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { createEditor } from "slate";
-import { Slate, withReact } from "slate-react";
-import { withHistory } from "slate-history";
-import { initialValue } from "../constants/initial-value";
+import { BaseEditor, Descendant, createEditor } from "slate";
+import { ReactEditor, Slate, withReact } from "slate-react";
+import { HistoryEditor, withHistory } from "slate-history";
+import { initialValue as INIT_VAL } from "../constants/initial-value";
 import { PhyllitePluginContainer } from "../types/phyllite-plugin";
 import { PluginsContext } from "../hooks/use-plugins";
 
@@ -10,15 +10,26 @@ type Props = {
   children: React.ReactNode;
   plugins?: PhyllitePluginContainer;
   pluginsDisabled?: boolean;
+  initialValue?: Descendant[];
+  editor?: BaseEditor & ReactEditor & HistoryEditor;
 };
 
-export const Phyllite = ({ children, plugins, pluginsDisabled }: Props) => {
+export const Phyllite = ({
+  children,
+  plugins,
+  pluginsDisabled,
+  initialValue,
+  editor: initEditor,
+}: Props) => {
   const editor = React.useMemo(
     () => withHistory(withReact(createEditor())),
     [],
   );
   return (
-    <Slate editor={editor} initialValue={initialValue}>
+    <Slate
+      editor={initEditor || editor}
+      initialValue={initialValue || INIT_VAL}
+    >
       <PluginsContext.Provider
         value={{
           plugins: plugins || {},
